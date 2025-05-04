4c89deb98537ca9b237d10ab081b3d94874a3232
barbute <jackhdemeterio@gmail.com>
2025-05-04 04:00:36 -0500

    04_removeFromArray solved
    NOTE If stuck take a break or just work through the problem logically

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...removable) {
+    let newList = [];
+    for (let i = 0; i < list.length; i++) {
+        let element = list[i];
+        let passChecks = true;
+        for (let j = 0; j < removable.length; j++) {
+            let removableElement = removable[j];
+            if (element === removableElement) {
+                passChecks = false;
+                // Since it fails the vibe check no point
+                // in continuing to check so break out of
+                // this loop
+                break;
+            }
+        }
+        if (passChecks) {
+            newList.push(element);
+        }
+    }
+    return newList;
 };
 
 // Do not edit below this line
