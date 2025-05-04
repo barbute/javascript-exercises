521a7e265e9f9dd29c5404bc801e0dfca2a7da15
barbute <jackhdemeterio@gmail.com>
2025-05-04 03:28:43 -0500

    03_reverseString solved
    NOTE Read docs about all built-in methods. I wrote code to complete an action in 15 lines for what could have been done in one

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let phraseArray = [];
+    let reveresedPhraseArray = [];
+    let reversedPhrase = "";
+    for (let i = 0; i < phrase.length; i++) {
+        phraseArray[i] = phrase.at(i);
+    }
+    for (let j = 0; j < phraseArray.length; j++) {
+        // Must subtract one since j starts at 0, thus if we try to subtract
+        // by j once it will go out of bounds trying to find the character
+        reveresedPhraseArray[j] = phraseArray[(phraseArray.length - 1) - j];
+    }
+    for (let h = 0; h < reveresedPhraseArray.length; h++) {
+        reversedPhrase += reveresedPhraseArray[h];
+    }
+    return reversedPhrase;
 };
 
 // Do not edit below this line
