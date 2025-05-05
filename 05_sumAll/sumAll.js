e488ca02606b6ec4dfaee2b587c94168997cba9b
barbute <jackhdemeterio@gmail.com>
2025-05-05 02:48:41 -0500

    05_sumAll solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR";
+    } else if (x < 0 || y < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let lowerBound = Math.min(x, y);
+    let upperBound = Math.max(x, y);
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
