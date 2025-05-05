b0351993a01a0df6c15b38c2fcde7aa30a3bf3d3
barbute <jackhdemeterio@gmail.com>
2025-05-05 03:04:19 -0500

    07_tempConversion solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  temperatureCelsius = (temperatureFahrenheit - 32.0) * (5.0 / 9.0);
+  return parseFloat(temperatureCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  temperatureFahrenheit = temperatureCelsius * (9.0 / 5.0) + 32.0;
+  return parseFloat(temperatureFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
