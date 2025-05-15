08f23d5d01b7bdab65381945553f827767cd9d57
barbute <jackhdemeterio@gmail.com>
2025-05-15 09:50:38 -0500

    08_calculator solved
    TODO Learn array.reduce();

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(n) {
+  /*
+   * Can also do n.reduce((tot, curr) => tot + curr, 0);
+   */
+	let sum = 0;
+  n.forEach((x) => {sum += x});
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(n) {
+  /*
+   * Can also do n.reduce((prod, curr) => prod * curr);
+   */
+  let product = 1;
+  n.forEach((x, index, array) => {product *= x})
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  /*
+   * Can also use Math.pow(x, y);
+   */
+	let result = 1;
+  for (let i = 0; i < y; i++) {
+    result = result * x;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let result = 1;
+  for (let i = 1; i <= x; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
