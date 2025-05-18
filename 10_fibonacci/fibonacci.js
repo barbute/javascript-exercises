4224143c0e2ebc0517415d2f28e5a4a953d42990
barbute <jackhdemeterio@gmail.com>
2025-05-18 15:58:28 -0500

    10_fibonacci solved
    NOTE Didn't need to use an array, could have just iterated and then return the final number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(memberIndex) {
+    if (memberIndex < 0) {
+        return "OOPS";
+    } else if (typeof(memberIndex) === "string") {
+        memberIndex = Number.parseInt(memberIndex);
+    }
+    let sequence = [0, 1];
+    let numOne = sequence[0];
+    let numTwo = sequence[1];
+    for (let i = 2; i <= memberIndex; i++) {
+        sequence[i] = numOne + numTwo;
+        numOne = numTwo;
+        numTwo = sequence[i];
+    }
+    return sequence[memberIndex];
 };
 
 // Do not edit below this line
