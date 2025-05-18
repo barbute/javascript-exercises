338653810b546c4ed77b299683007b2f7c061874
barbute <jackhdemeterio@gmail.com>
2025-05-18 15:21:49 -0500

    09_palindromes solved
    TODO Learn how to condense code by utilizing built-in methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let text = phrase.toLowerCase().split("")
+        .filter(function(value) {
+            if (value === "." || 
+                value === "," || 
+                value === " " || 
+                value === "!") {
+                return "";
+            } else {
+                return value;
+            }
+        });
+    let reversedText = text.toReversed();
+    let isPalindrome = true;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] !== reversedText[i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
 
+    return isPalindrome;
 };
 
 // Do not edit below this line
